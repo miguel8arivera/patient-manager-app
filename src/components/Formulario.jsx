9c6106a49c3463892f955dd8785a17ff906f0cd4
Miguel 8a <miguel.angel.8a.rivera@gmail.com>
2022-03-12 10:10:42 -0500

    useState add

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,32 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  const [appointment, setAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    hour: "",
+    symptom: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { pet, owner, date, hour, symptom } = appointment;
+
+  const hadlerSubmit = (e) => {
+    e.preventDefault();
+    if (
+      pet.trim() === "" ||
+      owner.trim() === "" ||
+      date.trim() === "" ||
+      hour.trim() === "" ||
+      symptom.trim() === ""
+    ) {
+      setError(true);
+
+      return;
+    }
+    console.log("sending...");
+  };
   return (
     <div>
       <h4>Create Appointment</h4>
-      <label htmlFor="pet">Pet Name:</label>
-      <input
-        type="text"
-        name="pet"
-        id="pet"
-        placeholder="Pet Name..."
-        className="u-full-width"
-      />
-      <label htmlFor="owner"> Owner's Name:</label>
-      <input
-        type="text"
-        name="owner"
-        id="owner"
-        placeholder="Owner's Name..."
-        className="u-full-width"
-      />
-      <label htmlFor="date">Date:</label>
-      <input type="date" name="date" id="date" className="u-full-width" />
-      <label htmlFor="hour">Hour:</label>
-      <input type="time" name="hour" id="hour" className="u-full-width" />
-      <label htmlFor="symptom">Symptom:</label>
-      <input type="text" name="symptom" id="symptom" className="u-full-width" />
-      <button className="u-full-width button-primary">Add Appointment</button>
+
+      {error && <p className="alerta-error">All fields are required</p>}
+
+      <form onSubmit={hadlerSubmit}>
+        <label htmlFor="pet">Pet Name:</label>
+        <input
+          type="text"
+          name="pet"
+          id="pet"
+          placeholder="Pet Name..."
+          className="u-full-width"
+          onChange={handleChange}
+          value={pet}
+        />
+        <label htmlFor="owner"> Owner's Name:</label>
+        <input
+          type="text"
+          name="owner"
+          id="owner"
+          placeholder="Owner's Name..."
+          className="u-full-width"
+          onChange={handleChange}
+          value={owner}
+        />
+        <label htmlFor="date">Date:</label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          className="u-full-width"
+          onChange={handleChange}
+          value={date}
+        />
+        <label htmlFor="hour">Hour:</label>
+        <input
+          type="time"
+          name="hour"
+          id="hour"
+          className="u-full-width"
+          onChange={handleChange}
+          value={hour}
+        />
+        <label htmlFor="symptom">Symptom:</label>
+        <input
+          type="text"
+          name="symptom"
+          id="symptom"
+          className="u-full-width"
+          onChange={handleChange}
+          value={symptom}
+        />
+        <button className="u-full-width button-primary">Add Appointment</button>
+      </form>
     </div>
   );
 };
